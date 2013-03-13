16d0383e7af79c01b8d94d4367b06e1b7c9f0935
Jordan Humphreys <jordan@mailyard.net>
2013-03-13 14:14:33 -0700

    Auto show for screens taller than 3050px

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -49,7 +49,12 @@ $(function ($) {
     } 
     if (scroll > 3050) {
       $("#2013").addClass("fade");
-    } 
+    }
+
+    if ($(this).height() > 3050) {
+      $('#1998, #1999, #2000, #2001, #2002, #2003, #2004, #2005, #2006, #2007, #2008, #2009, #2010, #2011, #2012, #2013')
+        .addClass('fade');
+    }
 	}).trigger('scroll');
 
   $("#submit").click(function(e) {
